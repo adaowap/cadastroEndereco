4ade07e6198a5401b255582dc05024290e53d2cf
adaowap <adao.filho@mercadolivre.com>
2023-04-24 21:11:00 -0300

    Commit inicial Implementación de la API de ViaCep para el registro de direcciones.

---
+++ src/test/java/br/com/meli/cadastroendereco/CadastroEnderecoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meli.cadastroendereco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroEnderecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
